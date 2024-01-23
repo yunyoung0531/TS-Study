e29025142679bcf44e09bffd987a3eeba72da855
yunyoung0531 <yunyoung0531@naver.com>
2024-01-23 17:16:04 +0900

    Narrowing

--- index.js
+++ index.js
@@ -59,3 +59,46 @@ function 결혼가능하냐(money, house, charm) {
     }
 }
 결혼가능하냐(700, false, '중');
+function 내함수(x) {
+    if (typeof x === 'number') {
+        return x + 1;
+    }
+    else if (typeof x === 'string') {
+        return x + 1;
+    }
+    else {
+        return 0;
+    }
+}
+// 숫자여러개를 array 자료에 저장해놨는데
+// 가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
+// 이걸 클리닝해주는 함수가 필요합니다. 
+// 클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
+// [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
+function 클리닝(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'string') {
+            result.push(+arr[i]);
+        }
+        else
+            result.push(arr[i]);
+    }
+    return result;
+}
+console.log(클리닝([123, '3']));
+var 철수쌤 = { subject: 'math' };
+var 영희쌤 = { subject: ['science', 'english'] };
+var 민수쌤 = { subject: ['science', 'art', 'korean'] };
+function 과목(x) {
+    if (typeof x.subject === 'string') {
+        return x.subject;
+    }
+    else if (Array.isArray(x.subject)) {
+        return x.subject[x.subject.length - 1];
+    }
+    else {
+        return '없음';
+    }
+}
+console.log(과목(민수쌤));
