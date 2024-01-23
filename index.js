68700a64bfc57c57ae8ae9e8375c7116ccca4f62
yunyoung0531 <yunyoung0531@naver.com>
2024-01-23 15:12:45 +0900

    타입 지정

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 var 이름 = 'choi';
+var 나이 = 24;
+var 고향 = "광주광역시";
+var 가수 = { songName: "newday", singer: "폴킴" };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
+var user = 'kim';
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
+};
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
 let 이름 :string = 'choi';
+let 나이 = 24;
+let 고향 = "광주광역시";
+
+let 가수 = { songName: "newday", singer: "폴킴"}
+
+let project :{ member : [string, string], days : number, started : boolean } = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
+}
+
+let user :string = 'kim';
+let age :(undefined | number) = undefined;
+let married :boolean = false; 
+let 철수 :(string | undefined | number | boolean)[] = [user, age, married];
+
+let 학교 :{ score : (number | boolean)[], teacher : string, friend : string | string[] } = {
+    score : [100, 97, 84],
+    teacher : 'Phil',
+    friend : 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher]
